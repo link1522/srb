7cb68c5de2c800f146a74316d11b2792cf5c3304
Link1515 <h26281660@gmail.com>
2025-08-25 16:15:31 +0800

    set config

---
+++ README.md
@@ -0,0 +1,5 @@
+將所有模組裝到本地倉庫
+`mvn clean install -DskipTests`
+
+執行 service-core
+`mvn -pl service-core spring-boot:run`
---
+++ service-core/src/main/java/com/example/srb/core/config/MybatisPlusConfig.java
@@ -0,0 +1,22 @@
+package com.example.srb.core.config;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+
+@Configuration
+@MapperScan("com.example.srb.core.mapper")
+@EnableTransactionManagement
+public class MybatisPlusConfig {
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+}
