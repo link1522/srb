9f1e432fa076494d51f3511c2c63285a4e1aedd4
Link1515 <h26281660@gmail.com>
2025-08-27 11:47:42 +0800

    set swagger api info

--- service-base/src/main/java/com/example/srb/base/config/Swagger2Config.java
+++ service-base/src/main/java/com/example/srb/base/config/Swagger2Config.java
@@ -3,6 +3,12 @@ package com.example.srb.base.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.google.common.base.Predicates;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -10,9 +16,41 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class Swagger2Config {
+    @Bean
+    public Docket AdminApiConfig() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("adminApi")
+                .apiInfo(adminApiInfo())
+                .select()
+                .paths(Predicates.and(PathSelectors.regex("/admin/.*")))
+                .build();
+    }
+
+    private ApiInfo adminApiInfo() {
+        return new ApiInfoBuilder()
+                .title("尚融寶後臺 API")
+                .description("描述後台各個 API 調用方式")
+                .version("1.6")
+                .contact(new Contact("Terry", "https://example.com", "haha@example.com"))
+                .build();
+    }
 
     @Bean
-    public Docket apiConfig() {
-        return new Docket(DocumentationType.SWAGGER_2);
+    public Docket webApiConfig() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("webApi")
+                .apiInfo(webApiInfo())
+                .select()
+                .paths(Predicates.and(PathSelectors.regex("/api/.*")))
+                .build();
+    }
+
+    private ApiInfo webApiInfo() {
+        return new ApiInfoBuilder()
+                .title("尚融寶前臺 API")
+                .description("描述前台各個 API 調用方式")
+                .version("1.6")
+                .contact(new Contact("Terry", "https://example.com", "haha@example.com"))
+                .build();
     }
 }
